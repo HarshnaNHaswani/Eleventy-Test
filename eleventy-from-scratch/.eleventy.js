8432548b67d561313c557c39794669273ca6956f
Harshna N Haswani <hharshna.n@gmail.com>
2021-11-27 19:07:40 +0530

    initial

---
+++ eleventy-from-scratch/.eleventy.js
@@ -0,0 +1,27 @@
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
+
+module.exports = config => {
+    config.addPassthroughCopy('./src/images/');
+    config.addWatchTarget('src');
+    // Returns work items, sorted by display order
+    config.addCollection('work', collection => {
+        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+    });
+  
+    // Returns work items, sorted by display order then filtered by featured
+    config.addCollection('featuredWork', collection => {
+        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+        x => x.data.featured
+        );
+    });
+    return {
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dir: {
+            input: 'src',
+            output: 'dist'
+        }
+    };
+  };
---
+++ eleventy-from-scratch/src/index.md
@@ -0,0 +1,22 @@
+---
+title: 'Hello, world'
+layout: 'layouts/home.html'
+intro:
+  eyebrow: 'Digital Marketing is our'
+  main: 'Bread & Butter'
+  summary: 'Let us help you create the perfect campaign with our multi-faceted team of talented creatives.'
+  buttonText: 'See our work'
+  buttonUrl: '/work'
+  image: '/images/bg/toast.jpg'
+  imageAlt: 'Buttered toasted white bread'
+primaryCTA:
+    title: 'This is an agency that doesn’t actually exist'
+    summary: 'This is the project site you build when you take the “Learn
+        Eleventy From Scratch” course so it is all made up as a pretend
+        context. You will learn a lot about Eleventy by building this site
+        though. Take the course today!'
+    buttonText: 'Buy a copy'
+    buttonUrl: 'https://learneleventyfromscratch.com'
+---
+
+This is pretty _rad_, right?
